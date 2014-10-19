a09203efd432b23871cd2a311878acf65e2df210
cwyark <cwyark@gmail.com>
2014-10-20 00:43:45 +0800

    fixed gae.pth path problem

--- app/templates/_main.py
+++ app/templates/_main.py
@@ -3,10 +3,10 @@ import site
 import sys <% if(virtualenv) { %>
 if hasattr(sys, 'real_prefix'):
     site.addsitedir(os.path.join(os.path.dirname(__file__), 'lib'))
-    gae_pth = os.environ.get('VIRTUAL_ENV') + '/lib/python2.7/site-packages' + '/gae.pth'
+    gae_pth = '%s/lib/python2.7/site-packages/gae.pth' % os.environ.get('VIRTUAL_ENV')
     if not os.path.isfile(gae_pth):
-        os.system('echo "<%= gcloud_path %>/platform/google_appengine" >> %s' % gae_pth);
-        os.system('echo "import dev_appserver; dev_appserver.fix_sys_path()" >> %s' % gae_pth);
+        os.system('echo "<%= gcloud_path %>/platform/google_appengine" >> %s' % gae_pth)
+        os.system('echo "import dev_appserver; dev_appserver.fix_sys_path()" >> %s' % gae_pth)
     try:
         import app
     except ImportError, err:
