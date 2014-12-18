5d3434c4b9dee18198fcb5ba3fec53e5a2e5ba68
Chester <cwyark@gmail.com>
2014-12-18 23:43:31 +0800

    add Gruntfile.js contents

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -1,5 +1,25 @@
 'use strict';
 
 module.exports = function (grunt) {
-
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		compass: {
+			dist: {
+				options: {
+					sassDir: '<%= pkg.static_folder %>/{sass,scss}',
+					cssDir: '<%= pkg.static_folder %>/{css,stylesheet}'
+				}
+			}
+		},
+		watch: {
+			css: {
+				files: '**/*.{scss,sass}',
+				tasks: ['compass']
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-compass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.registerTask('default', ['watch']);
 };
