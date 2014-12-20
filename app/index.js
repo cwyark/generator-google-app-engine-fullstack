03251c1bb0ead26302d587673a4a1d75d0577a87
Chester <cwyark@gmail.com>
2014-12-20 11:55:11 +0800

    1. add filter in index.js 2. use this.pkg as configs 3. fixed this.filter.compass could not pass to package.json

--- app/index.js
+++ app/index.js
@@ -10,6 +10,7 @@ var gruntfile = require('gruntfile-editor');
 var GAEFullstackGenerator = yeoman.generators.Base.extend({
   	initializing: function () {
     	this.pkg = require('../package.json');
+		this.filter = {};
   	},
 	
 	info: function() {
@@ -30,8 +31,8 @@ var GAEFullstackGenerator = yeoman.generators.Base.extend({
 			message: 'Which python framework do you want to use?',
 			choices:['flask','webapp2','bottle'],
 		}], function (props) {
-			this.app_id 				= props.app_id;
-			this.PyFramework 			= props.PyFramework;
+			this.app_id 			= props.app_id;
+			this.filter.PyFramework = props.PyFramework;
 			done();
 		}.bind(this));
   	},
@@ -41,7 +42,7 @@ var GAEFullstackGenerator = yeoman.generators.Base.extend({
 		this.prompt([
 		{
 			type: 'confirm',
-			name: 'Compass',
+			name: 'compass',
 			message: 'Do you want to use Compass?',
 			default: false
 		}, {
@@ -50,26 +51,27 @@ var GAEFullstackGenerator = yeoman.generators.Base.extend({
 			message: 'Which UI framework do you want to use?',
 			choices: ['bootstrap', 'Foundation', 'Semantic', 'None'],
 		}], function (props) {
-			this.UiFramework = props.UiFramework;
-			this.compass = props.compass;
+			this.filter.compass 	= props.compass;
+			console.log(this.filter.compass);
+			this.filter.UiFramework = props.UiFramework;
 			done();
 		}.bind(this));
   	},
 
   writing: {
     app: function () {
-      this.dest.mkdir('app');
-      this.dest.mkdir('lib');
-      this.dest.mkdir('templates');
-      this.dest.mkdir('static');
+      this.dest.mkdir(this.pkg.gae_project.template_folder);
+      this.dest.mkdir(this.pkg.gae_project.static_folder);
+      this.dest.mkdir(this.pkg.gae_project.gae_app);
+      this.dest.mkdir(this.pkg.gae_project.gae_lib);
+
 	  this.src.copy('appengine_config.py','appengine_config.py');
 	  this.src.copy('Gruntfile.js','Gruntfile.js');
 	  this.src.copy('bowerrc.json','.bowerrc');
 	  this.src.copy('gitignore','.gitignore');
-	  this.src.copy('pyweb/' + this.PyFramework + '_entry.py','main.py');
-	  this.template('_app.yaml', 'app.yaml');
-	  /* pip's requirements config */
+	  this.src.copy('pyweb/' + this.filter.PyFramework + '_entry.py','main.py');
 
+	  this.template('_app.yaml', 'app.yaml');
 	  this.template('_requirements.txt', 'requirements.txt');
 	  this.template('_bower.json', 'static/bower.json');
 	  this.template('_package.json', 'package.json');
